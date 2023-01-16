d3db59e5329a1153bf5dc06ee500c183daec8a18
karanmaurya15 <karanmaurya9663@gmail.com>
2023-01-16 22:37:38 +0530

    [Karan] Insert Contact to table

---
+++ AddressBook_LINQ/AddressBook_LINQ/AddressBookManagment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook_LINQ
+{
+    internal class AddressBookManagment
+    {
+        DataTable table = new DataTable();
+        public void InsertContactToTable()
+        {
+            table.Columns.Add("FirstName", typeof(string));  
+            table.Columns.Add("LastName", typeof(string));
+            table.Columns.Add("Address", typeof(string));
+            table.Columns.Add("City", typeof(string));
+            table.Columns.Add("State", typeof(string));
+            table.Columns.Add("Zip", typeof(int));
+            table.Columns.Add("PhoneNumber", typeof(long));
+            table.Columns.Add("Email", typeof(string));
+            table.Rows.Add("Shubham", "Singh", "Vanaras", "City", "UttarPradesh", 222129, 9876543210, "shubham@gmail.com");
+            table.Rows.Add("Karan", "Kushwaha", "Gorakhpur", "Lucknow", "Uttar Pradesh", 675432, 8765432190, "karan@gmail.com");
+            table.Rows.Add("Irbaz", "Patel", "Bharuch", "Anywhere", "Gujrat", 654328, 6543210987, "irbaz@gmail.com.");
+        }
+    }
+}
---
+++ AddressBook_LINQ/AddressBook_LINQ/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AddressBook_LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Address Book System Using LINQ");
+            AddressBookManagment addressBookManagment = new AddressBookManagment();
+            addressBookManagment.InsertContactToTable();
+        }
+    }
+}
